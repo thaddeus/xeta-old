7ed73eb52668d06488ade55714703b528a6d0343
Thaddeus Bond <thaddeus@thaddeusbond.com>
2013-12-05 12:47:40 -0500

    Basic changes

--- XETA/XETA/My Project/Application.Designer.vb
+++ XETA/XETA/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.XETA.Form1
+            Me.MainForm = Global.XETA.mainGUI
         End Sub
     End Class
 End Namespace
---
+++ XETA/XETA/mainGUI.vb
@@ -0,0 +1,3 @@
+﻿Public Class mainGUI
+
+End Class
